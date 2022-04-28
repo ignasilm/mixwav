70dbe9737adcdf644b49a8129c5002153f7fa053
Ignacio Lopez Matinez <ignasilm@quehacenhoy.com>
2022-04-28 21:05:30 +0200

    Version inicial

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # mixwav
  Fusion varios wav en uno solo
+
+---
+
+## Descripción
+
+El motivo de esta herramienta es para fusionar/mezclar varios ficheros wav en uno solo. La necesidad surgió al usar la IA [Demucs](https://github.com/facebookresearch/demucs), que separa en varios ficheros wav la voz y ciertos  instrumentos, a partir de un fichero de audio. En mi caso la utilicé para eliminar el sonido del viento de una grabacion de video. Como Demucs separa una pista con la voz, me permitía recuperar por separado la voz del resto de sonidos. Pero como casi todo el sonido del viento lo enviaba a la misma pista, quise volver a unir el resto de pistas con la de la voz para tener algo de sonido ambiente pero sin el ruido molesto del viento. Así que necesitaba volver a fusionar otra vez los ficheros wav que habia generado Demucs sin el fichero del viento.
+
+---
+
+## Instalación en local
+
+1. Instalar Python 3.8+ desde [python.org](https://www.python.org/downloads/)
+
+2. crear directorio:
+
+	`cd c:\Dev\Python\workspace`
+	
+	`mkdir mixwav`
+	
+	`cd mixwav`
+	
+3. crear entorno virtual
+	
+	`pip install virtualenv`
+	
+	`virtualenv env`
+	
+4. Activar el entorno virtual
+	desde C:\Dev\Python\workspace\mixwav ejecutar:
+	
+	`c:/Dev/Python/workspace/mixwav/env/Scripts/activate.ps1`
+	
+5. Copiar codigo de mixwav en C:\Dev\Python\workspace\mixwav
+
+6. instalar librerias
+
+	`pip install -r .\requirements.txt`
+	
+7. Ejecutar aplicacion
+
+	`c:/Dev/Python/workspace/mixwav/env/Scripts/python.exe c:/Dev/Python/workspace/mixwav/mixwav.py`
+
+
+## Uso
+~~~
+usage: mixwav.py [-h] -i INPUT [-o OUTPUT]
+
+Utilidad para fusionar varios ficheros wav en un solo archivo.
+
+optional arguments:
+  -h, --help            Muestra esta información y termina la ejecución.
+  -i INPUT, --input INPUT
+                        Path que contiene los wav que hay que fusionar
+  -o OUTPUT, --output OUTPUT
+                        Nombre del fichero de salida. Por defecto será mixwav.wav
+
+Ejemplo de uso:
+    Para fusionar todos los wav dentro de un path e indicar el fichero de salida:
+                    mixwav -i "C:\videos\Viaje Alicante\audios_corte_1" -o "C:\videos\Viaje Alicante\GOPRO\voz_corte_1.wav"
+~~~
---
+++ mixwav.py
@@ -0,0 +1,56 @@
+from pydub import AudioSegment
+import argparse
+import textwrap
+from os import chdir
+import glob
+
+print(' ')
+ap = argparse.ArgumentParser(add_help=False, 
+                             description='Utilidad para fusionar varios ficheros wav en un solo archivo.',
+                             formatter_class=argparse.RawDescriptionHelpFormatter,
+                             epilog=textwrap.dedent('''\
+                                Ejemplo de uso:
+                                    Para fusionar todos los wav dentro de un path e indicar el fichero de salida: 
+                                                    mixwav -i "C:\\videos\\Viaje Alicante\\audios_corte_1" -o "C:\\videos\\Viaje Alicante\\GOPRO\\voz_corte_1.wav"
+                                    ''' ))
+ap.add_argument('-h', '--help', action='help', help='Muestra esta información y termina la ejecución.')
+ap.add_argument('-i', '--input', required=True, help='Path que contiene los wav que hay que fusionar')
+ap.add_argument('-o', '--output', required=False, help='Nombre del fichero de salida. Por defecto será mixwav.wav')
+args = vars(ap.parse_args())
+
+
+
+if args['input'] is not None:
+    path = args['input']
+
+salida = './mixwav.wav'
+if args['output'] is not None:
+    salida = args['output']
+
+
+chdir(path)
+
+#Recuperamos los ficheros de la carpeta que cumplen el patron
+lista_ficheros = glob.glob('*.wav')
+
+if len(lista_ficheros) < 2:
+    print('No hay suficientes ficheros *.wav para fusionar.')
+else:
+    #Recuperamos un fichero como inicial al que ir añadiendo el resto de pistas
+    print('Fichero inicial:', lista_ficheros[0])
+    combined = AudioSegment.from_file(lista_ficheros[0])
+    print('     Número de canales:', combined.channels)
+    print('     Bits por Sample:', combined.sample_width * 8)
+    print('     Frame rate:', combined.frame_rate)
+    print('     Duración (segundos):', combined.duration_seconds)
+    print(' ')
+    
+    #Recorremos el resto de ficheros 
+    for file_name in lista_ficheros[1:]:
+        print('Procesando el fichero:', file_name)
+        mix = AudioSegment.from_file(file_name)
+        combined = combined.overlay(mix)
+
+combined.export(salida, format='wav')
+print(' ')
+print('El fichero final se ha generado en:', salida)
